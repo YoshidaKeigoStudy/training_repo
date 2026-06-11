2f71bf0d518a79ea5b72a46f65d00ea60b6bb075
DESKTOP-FBOH95S\yoshida <yoshida@cosmo-w.co.jp>
2026-06-11 13:58:13 +0900

    3人用それぞれのhtmlを作成するように仕様変更

--- src/main/java/com/example/training/controller/HomeController.java
+++ src/main/java/com/example/training/controller/HomeController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    private static final String MEMBER_1 = "A";
-    private static final String MEMBER_2 = "B";
-    private static final String MEMBER_3 = "C";
+    private static final String MEMBER_1 = "ここに名前を記入";
+    private static final String MEMBER_2 = "ここに名前を記入";
+    private static final String MEMBER_3 = "ここに名前を記入";
 
     @GetMapping("/")
     public String index(Model model) {
@@ -17,5 +17,20 @@ public class HomeController {
         model.addAttribute("member2", MEMBER_2);
         model.addAttribute("member3", MEMBER_3);
         return "index";
+    }
+
+    @GetMapping("/member1")
+    public String member1() {
+        return "kajimura";
+    }
+
+    @GetMapping("/member2")
+    public String member2() {
+        return "hamano";
+    }
+
+    @GetMapping("/member3")
+    public String member3() {
+        return "yoshimura";
     }
 }
